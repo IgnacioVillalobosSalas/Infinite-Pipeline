d54c1a173f2b20a5416e02ad52022ca055a642bf
Ignacio Villalobos Salas <nachovs91@outlook.es>
2016-08-20 02:05:02 +0200

    infinite runner in pipes , dodge obstacles and try to get the best score

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+	public Text distanceLabel, velocityLabel;
+
+	public void SetValues (float distanceTraveled, float velocity) {
+		distanceLabel.text = "Score : " + ((int)(distanceTraveled * 10f)).ToString();
+		velocityLabel.text = "Speed : " + ((int)(velocity * 10f)).ToString();
+	}
+}
